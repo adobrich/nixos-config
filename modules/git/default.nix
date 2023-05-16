c942adb3894077039e32002c6b4a82973babccec
Andrew Dobrich <andrew.dobrich@proton.me>
2023-05-16 20:50:35 +1000

    git module

---
+++ modules/git/default.nix
@@ -0,0 +1,23 @@
+{pkgs, lib, config, ...}:
+with lib;
+let cfg = config.modules.git;
+in {
+  options.modules.git = { enable = mkEnableOption "git"; };
+  config = mkIf cfg.enable {
+    programs.git = {
+      enable = true;
+      userName = "Andrew Dobrich";
+      userEmail = "andrew.dobrich@proton.me";
+      extraConfig = {
+        init = { defaultBranch = "main"; };
+        core = { excludesfile = "$NIXOS_CONFIG_DIR/scripts/gitignore"; };
+      };
+    };
+
+    # Use libreSSL and start the key agent
+    programs.ssh = {
+      enable = true;
+      package = pkgs.libressl;
+      startAgent = true;
+  };
+}
