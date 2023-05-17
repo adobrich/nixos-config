ee0beca8b23852362bbc240d1370635805a2de7f
Andrew Dobrich <andrew.dobrich@protonmail.com>
2023-05-17 20:43:46 +1000

    added brave extensions

--- modules/brave/default.nix
+++ modules/brave/default.nix
@@ -7,12 +7,18 @@
 with lib; let
   cfg = config.modules.brave;
 in {
-  options.modules.brave= {enable = mkEnableOption "brave";};
+  options.modules.brave = {enable = mkEnableOption "brave";};
   config = mkIf cfg.enable {
     home.packages.chromium = {
       enable = true;
       package = pkgs.brave;
+
+      extensions = [
+        "nngceckbapebfimnlniiiahkandclblb" # Bitwarden
+        "nplimhmoanghlebhdiboeellhgmgommi" # Tab Groups
+        "eimadpbcbfnmbkopoojfekhnkhdbieeh" # Dark Reader
+        "pkehgijcmpdhfbdbbnkijodmdjhbjlgp" # Privacy Badger
+      ];
     };
-    # TODO: add extensions
   };
 }
