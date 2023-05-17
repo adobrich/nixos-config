c7b2764b871e461359988d6e7ac2bd7e1e1cb5bb
Andrew Dobrich <andrew.dobrich@proton.me>
2023-05-17 17:28:16 +1000

    inital pinebook-pro setup

--- hosts/pinebook-pro/default.nix
+++ hosts/pinebook-pro/default.nix
@@ -1,1 +1,21 @@
-{...}: {}
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}: {
+  imports = [
+    # Host hardware configuration
+    # Kernel, bootloader and filesystem config
+    ./hardware-configuration.nix
+    # Hardware tweaks/quirks from nixos-hardware
+    inputs.hardware.nixosModules.pine64-pinebook-pro
+    inputs.hardware.nixosModules.common-pc-laptop-ssd
+
+    # Global Software...
+
+    # Users
+    ../../users/andy
+    ../../users/root
+  ];
+}
--- hosts/pinebook-pro/hardware-configuration.nix
+++ hosts/pinebook-pro/hardware-configuration.nix
@@ -1,1 +1,61 @@
-{...}: {}
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  boot = {
+    initrd.availableKernelModules = [ "nvme" "usbhid" ];
+    initrd.kernelModules = [ ];
+    kernelModules = [ ];
+    extraModulePackages = [ ];
+    kernelParams = [ "quiet" ];
+
+    plymouth.enable = true;
+
+    loader = {
+      systemd-boot = {
+        enable = true;
+        consoleMode = "max";
+      };
+      efi.canTouchEfiVariables = true;
+    };
+  };
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-label/nixos";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-label/boot";
+      fsType = "vfat";
+    };
+
+  fileSystems."/home" =
+    { device = "/dev/disk/by-label/home";
+      fsType = "ext4";
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-label/swap"; }
+    ];
+
+  powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
+
+  hardware = {
+    opengl = {
+      enable = true;
+      driSupport = true;
+      # TODO: 32 bit dri support
+    };
+  };
+
+  environment.sessionVariables = {
+    PAN_MESA_DEBUG="gl3";
+  };
+
+  nixpkgs.hostPlatform = "aarch64-linux";
+}
+
