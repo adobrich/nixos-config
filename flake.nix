0bae9fbbb027d574393acfc7a36832ae34536f13
Andrew Dobrich <andrew.dobrich@protonmail.com>
2024-04-05 08:41:09 +1100

    23.11

--- flake.nix
+++ flake.nix
@@ -41,7 +41,7 @@
   } @ inputs: let
     inherit (self) outputs;
     # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-    stateVersion = "24.05";
+    stateVersion = "23.11";
 
     # Functions to generate packages for each system stolen from Misterio77... thanks!
     # https://github.com/Misterio77/nix-config
